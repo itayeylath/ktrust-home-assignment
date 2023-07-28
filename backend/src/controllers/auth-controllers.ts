b92d865c7e1b722312ff8fc14edbbb8eb629a5a1
itayeylath <itay.ey@gmail.com>
2023-07-28 19:14:11 +0300

    Add auth-controllers.ts

---
+++ backend/src/controllers/auth-controllers.ts
@@ -0,0 +1,83 @@
+import express from "express";
+import { JWTsign, authentication, random } from "../helpers/users-helpers";
+import { createUser, getUserByEmail } from "../models/users-models";
+
+// Register New user
+export const register = async (req: express.Request, res: express.Response) => {
+    try {
+        const { email, password, username } = req.body;
+        if (!email || !password || !username) {
+            return res.sendStatus(400);
+        }
+
+        const isExistUser = await getUserByEmail(email);
+        if (isExistUser) {
+            return res.sendStatus(400);
+        }
+
+        const salt = random();
+        const user = await createUser({
+            email,
+            username,
+            authentication: {
+                salt,
+                password: authentication(salt, password),
+            },
+        });
+
+        return res.status(200).json(user).end();
+    } catch (err) {
+        return res.sendStatus(400).json({ massage: err.massage });
+    }
+};
+
+// Log in exist user
+export const logIn = async (req: express.Request, res: express.Response) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password || email === "" || password === "") {
+            return res.sendStatus(400);
+        }
+
+        const user = await getUserByEmail(email).select(
+            "+authentication.salt +authentication.password"
+        );
+
+        if (!user) {
+            return res.sendStatus(400);
+        }
+
+        const expectedHash = authentication(user.authentication.salt, password);
+
+        if (user.authentication.password != expectedHash) {
+            return res.sendStatus(403);
+        }
+
+        const salt = random();
+        const token = JWTsign(user.username, salt);
+
+        user.authentication.sessionToken = token;
+
+        await user.save();
+
+        res.cookie("auth-cookie-itay", user.authentication.sessionToken, {
+            domain: "localhost",
+            path: "/",
+        });
+
+        return res.status(200).json(user).end();
+    } catch (err) {
+        return res.sendStatus(400).json({ massage: err.massage });
+    }
+};
+
+//Logout
+export const logOut = async (_req: express.Request, res: express.Response) => {
+    try {
+        res.clearCookie("auth-cookie-itay");
+        return res.status(200).end();
+    } catch (err) {
+        return res.sendStatus(400).json({ massage: err.massage });
+    }
+};
