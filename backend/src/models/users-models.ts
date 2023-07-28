303ac18eac2da8818a6fe68b8167c71762360808
itayeylath <itay.ey@gmail.com>
2023-07-28 19:10:04 +0300

    Add users-models.ts

---
+++ backend/src/models/users-models.ts
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true
+    },
+     authentication: {
+        password: {
+            type: String,
+            required: true,
+            select: false
+        },
+        salt: {
+            type: String,
+            select: false
+        },
+        sessionToken: {
+            type: String,
+            select: false
+        }
+     }
+
+})
+
+export const UserModel = mongoose.model('User',userSchema )
+
+// User Model CRUD functions
+export const getUsers = () => {
+    return  UserModel.find()
+};
+export const getUserByEmail = (email: String) => {
+    return UserModel.findOne({email})
+};
+export const getUserBySessionToken = async (seessionToken: String) => {
+    return UserModel.findOne({"authentication.sessionToken": seessionToken})
+};
+export const getUserById = (id: string) => {
+    return UserModel.findOne({_id: id})
+};
+export const createUser = async (values: Record<string, any>) => {
+    return new UserModel(values).save().then((user) => user.toObject())
+};
+export const deleteUserById = (id: string) => {
+    return UserModel.findOneAndDelete({_id: id})
+};
+export const updateUserById = (id: String, values: Record<string, any>) => {
+    return UserModel.findOneAndUpdate(id, values)
+};
