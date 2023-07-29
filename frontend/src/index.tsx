f201e3d839b5f1cd9628a23b1b5f63d6bcaaaabd
itayeylath <itay.ey@gmail.com>
2023-07-29 12:54:40 +0300

    Add sign-up-page and Redux

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './styles/_main.scss';
 import { store } from './store/index';
 import { Provider } from 'react-redux';
-import './styles/_main.scss';
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
 	<Provider store={store}>
--- frontend/src/routes/main.routes.tsx
+++ frontend/src/routes/main.routes.tsx
@@ -2,11 +2,13 @@ import { Route, Routes } from "react-router-dom";
 import { Login } from "../pages/login";
 import HomePageAdmin from "../pages/ home-page-admin";
 import HomePage from "../pages/ home-page";
+import { Signup } from "../pages/signup";
 
 export const MainRoutes = () => {
     return (
         <Routes>
             <Route index element={<Login />} />
+            <Route path="signup" element={<Signup />} />
             <Route path="home/admin" element={<HomePageAdmin />} />
             <Route path="home/" element={<HomePage />} />
         </Routes>
