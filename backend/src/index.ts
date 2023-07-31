f2fa2988f6b33c731be4a0bd81fc76e66836feb5
itayeylath <itay.ey@gmail.com>
2023-07-31 14:45:55 +0300

    ADD PORT and MONGO_URL to .env file

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,11 @@
 import { startServer } from "./server/index-server";
 import { connectDb } from "./db/index-db";
 import express from "express";
+require("dotenv").config();
 
-const MONGO_URL = "mongodb://localhost/users-test";
-const port = 3000;
 export const app = express();
 
-startServer(port, app);
-connectDb(MONGO_URL);
+startServer(process.env.PORT, app);
+connectDb(process.env.MONGO_URL);
 
 export default app;
